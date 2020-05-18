ae2cb317fb9676226b6aafa15edcbfea4a1d67fa
lodhaad <aditya.lodha@gmail.com>
2020-05-18 11:56:55 +0800

    Initial commit

---
+++ src/main/java/com/tradeai/supplysettlementsservice/SupplySettlementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tradeai.supplysettlementsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplySettlementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplySettlementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradeai/supplysettlementsservice/SupplySettlementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradeai.supplysettlementsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplySettlementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
